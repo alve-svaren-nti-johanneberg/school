c2df370fd7e4cdb8c7b6cd74564045344c9e5b1c
Alve Svarén <alve@hotmail.se>
2020-10-16 10:38:19 +0200

    Added smallest_of_three function

---
+++ programmering1/overleaf_uppgifter/smallest_of_three.rb
@@ -0,0 +1,10 @@
+require "./smallest_of_two.rb"
+
+def smallest_of_three(num1, num2, num3)
+    if num1 < num2 && num1 < num3
+        return num1
+    elsif num2 < num1 && num2 < num3
+        return num2
+    end
+    return num3
+end

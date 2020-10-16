cb87fcaa520568508a6b07234ebdd64149ac20bb
Alve Svarén <alve@hotmail.se>
2020-10-16 14:53:49 +0200

    Redid smallest of three

--- programmering1/overleaf_uppgifter/smallest_of_three.rb
+++ programmering1/overleaf_uppgifter/smallest_of_three.rb
@@ -1,10 +1,6 @@
 require "./smallest_of_two.rb"
 
 def smallest_of_three(num1, num2, num3)
-    if num1 < num2 && num1 < num3
-        return num1
-    elsif num2 < num1 && num2 < num3
-        return num2
-    end
-    return num3
+    small = smallest_of_two(num1, num2)
+    return small < num3 ? small : num3
 end

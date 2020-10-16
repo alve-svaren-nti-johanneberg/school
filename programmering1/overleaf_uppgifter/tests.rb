cb0a9f46dc0f70ef57179e485d2958df8ca1b926
Alve Svarén <alve@hotmail.se>
2020-10-16 11:04:07 +0200

    Added square test

--- programmering1/overleaf_uppgifter/tests.rb
+++ programmering1/overleaf_uppgifter/tests.rb
@@ -5,3 +5,7 @@ fail unless next_number(104) == 105
 require "./previous_number"
 fail unless previous_number(104) == 103
 fail unless previous_number(-88) == -89
+
+require "./square"
+fail unless square(3) == 9
+fail unless square(-83477) == 6968409529

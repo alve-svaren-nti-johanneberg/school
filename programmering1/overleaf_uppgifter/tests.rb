35c7b0fef81fb1173ec73b4af60b7ba738c9e1bf
Alve Svarén <alve@hotmail.se>
2020-10-16 11:02:24 +0200

    Added previous number test

--- programmering1/overleaf_uppgifter/tests.rb
+++ programmering1/overleaf_uppgifter/tests.rb
@@ -2,3 +2,6 @@ require "./next_number"
 fail unless next_number(-88) == -87
 fail unless next_number(104) == 105
 
+require "./previous_number"
+fail unless previous_number(104) == 103
+fail unless previous_number(-88) == -89
